b928f39e9aa01147c5adb5e08a16ce27618f585c
amosh443 <mr.almosh443@mail.ru>
2019-01-21 14:18:06 +0300

    viewsets

--- proj/app/views.py
+++ proj/app/views.py
@@ -1,6 +1,35 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from proj.app import models
+from proj.app import serializers
+from rest_framework import viewsets
 
-from django.shortcuts import render
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class CafeViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows cafes to be viewed or edited.
+    """
+    queryset = models.Cafe.objects.all()
+    serializer_class = serializers.CafeSerializer
+
+
+class DishViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows dishes to be viewed or edited.
+    """
+    queryset = models.Dish.objects.all()
+    serializer_class = serializers.DishSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows orders to be viewed or edited.
+    """
+    queryset = models.Order.objects.all()
+    serializer_class = serializers.CafeSerializer
